49c033a36e4b9dfbb620a7be57ccdc6f22e09c95
Omar Meriwani <omar.sirwan@gmail.com>
2017-05-29 14:19:00 +0300

    --

---
+++ TivoliClass.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Dapper;
+using System.Data;
+using System.Configuration;
+using System.Data.OracleClient;
+using System.ServiceModel;
+using Shortcodes.MXSRRef;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Shortcodes.Controllers
+{
+    public class TivoliClass
+    {
+
+        public static string SendTicket(string CreatedBy, string ReportedBy,string Shortcode, string description, string operation, string scid, string username, string password)
+        {
+            try 
+            {
+                //2400	Change Rate	CHANGE RATE
+                //2399	Remove Short Code	REMOVE SC
+                //2398	NewService_IVR_SMS	IVR_SMS
+                //2396	NewService_SMS	SMS
+                //2397	NewService_IVR	IVR
+                int classid = 0;
+                string summary = "";
+                if (operation == "CHANGE RATE")
+                {
+                    classid = 2400;
+                    summary = "Change Shortcode Rate #" + Shortcode;
+                }
+                else if (operation == "REMOVE")
+                {
+                    classid = 2399;
+                    summary = "Remove shortcode #" + Shortcode;
+                }
+                else if (operation == "IVR_SMS")
+                {
+                    classid = 2398;
+                    summary = "Create IVR SMS Shortcode #" + Shortcode;
+
+                }
+                else if (operation == "SMS")
+                {
+                    classid = 2396;
+                    summary = "Create SMS Shortcode #" + Shortcode;
+                }
+                else if (operation == "IVR")
+                {
+                    classid = 2397;
+                    summary = "Create IVR Shortcode #" + Shortcode;
+                }
+
+                description = description + "\nService link: https://enterprise.korektel.com/Shortcodes/Service/Details/" + scid.ToString();
+                MXSRRef.MXSR j = new MXSRRef.MXSR();
+                j.Credentials = new NetworkCredential(username, password);
+                ByPassCert.BypassCertificateError();
+                j.Url = "APPLICATION_URL/meaweb/wsdl/MXSR?wsdl";
+                List<MXSRRef.MXSR_SRType> t = new List<MXSRRef.MXSR_SRType>();
+                MXSRRef.MXSR_SRType k = new MXSRRef.MXSR_SRType();
+                k.ACTLABCOST = new MXSRRef.MXDoubleType() { Value = 0 };
+                k.ACTLABHRS = new MXSRRef.MXDoubleType() { Value = 0 };
+                k.OWNER = new MXSRRef.MXStringType() { Value = CreatedBy.ToUpper() };
+                k.CHANGEDATE = new MXSRRef.MXDateTimeType() { Value = DateTime.Now };
+                k.CREATEDBY = new MXSRRef.MXStringType() { Value = CreatedBy.ToUpper() };
+                k.CREATIONDATE = new MXSRRef.MXDateTimeType() { Value = DateTime.Now };
+                k.REPORTEDBY = new MXSRRef.MXStringType() { Value = ReportedBy.ToUpper() };
+                k.CCVOUCHERSN3 = new MXSRRef.MXStringType() { Value = scid };
+                k.SITEID = new MXSRRef.MXStringType() { Value = "site_it" };
+                k.DESCRIPTION = new MXSRRef.MXStringType() { Value = summary };
+                k.DESCRIPTION_LONGDESCRIPTION = new MXSRRef.MXStringType() { Value = description };
+                k.ASSIGNEDOWNERGROUP = new MXSRRef.MXStringType() { Value = "MKT" };
+                k.OWNERGROUP = new MXSRRef.MXStringType() { Value = "MKT" };
+                k.EXTERNALSYSTEM = new MXSRRef.MXDomainType() { Value = "SHORTCODE" };
+                t.Add(k);
+                DateTime d = DateTime.Now;
+                string reflang = "";
+                string translang = "";
+                string no = "";
+                string maximoVersiom = "";
+                bool uiui = true;
+                MXSRRef.SRKeyType[] kh = j.CreateMXSR(t.ToArray(), ref d, ref uiui, ref reflang, ref   translang, ref  no, ref  maximoVersiom);
+                string nooo = kh[0].TICKETID.Value;
+                string nooo = "";
+                return nooo;
+            }
+            catch (System.Net.WebException exx)
+            { throw exx; }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                string km = ex.InnerException.Message;
+                throw ex;
+            }
+            
+        }
+        public static string SendTicket(string Shortcode, string description, string operation, string scid, string username, string password)
+        {
+            //2400	Change Rate	CHANGE RATE
+            //2399	Remove Short Code	REMOVE SC
+            //2398	NewService_IVR_SMS	IVR_SMS
+            //2396	NewService_SMS	SMS
+            //2397	NewService_IVR	IVR
+            int classid = 0;
+            string summary = "";
+            if (operation == "CHANGE RATE")
+            {
+                classid = 2400;
+                summary = "Change Shortcode Rate #" + Shortcode;
+            }
+            else if (operation == "REMOVE")
+            {
+                classid = 2399;
+                summary = "Remove shortcode #" + Shortcode;
+            }
+            else if (operation == "IVR_SMS")
+            {
+                classid = 2398;
+                summary = "Create IVR SMS Shortcode #" + Shortcode;
+
+            }
+            else if (operation == "SMS")
+            {
+                classid = 2396;
+                summary = "Create SMS Shortcode #" + Shortcode;
+            }
+            else if (operation == "IVR")
+            {
+                classid = 2397;
+                summary = "Create IVR Shortcode #" + Shortcode;
+            }
+
+            description = description + "\nService link: /Service/Details/" + scid.ToString();
+            try
+            {
+                System.Net.ServicePointManager.DefaultConnectionLimit = 50;
+                System.Net.WebClient wc = new System.Net.WebClient();
+                HttpWebRequest wbRequest = (HttpWebRequest)WebRequest.Create(new Uri(@"SERVICE_URL/services/MXSR"));
+                wbRequest.Credentials = new NetworkCredential("USER", "PASSWORD");
+                string dateString = String.Format("{0:s}", DateTime.Now);
+                wbRequest.Timeout = 50000;
+                byte[] bytes;
+                string requestXml = @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:max=""http://www.ibm.com/maximo""> 
+   <soapenv:Header/> 
+   <soapenv:Body> 
+      <max:CreateMXSR> 
+         <max:MXSRSet> 
+            <!--Zero or more repetitions:--> 
+            <max:SR> 
+               <!--Optional:--> 
+              <max:ASSIGNEDOWNERGROUP changed=""?"">MKT</max:ASSIGNEDOWNERGROUP>
+              <max:OWNERGROUP changed=""?"">MKT</max:OWNERGROUP>
+              <max:EXTERNALSYSTEM changed=""?"">SHORTCODE</max:EXTERNALSYSTEM>
+              <max:SITEID changed=""?"">site_it</max:SITEID>
+              <max:DESCRIPTION changed=""?"">" + summary + @"</max:DESCRIPTION>
+                <max:CCVOUCHERSN3 changed=""?"">"+ scid +@"</max:CCVOUCHERSN3>
+               <max:CREATEDBY changed=""?"">" + username + @"</max:CREATEDBY>
+               <max:DESCRIPTION_LONGDESCRIPTION changed=""?"">" + description + @"</max:DESCRIPTION_LONGDESCRIPTION>
+               <max:OWNER changed=""?"">" + username + @"</max:OWNER>
+               <max:REPORTEDBY changed=""?"">" + username +@"</max:REPORTEDBY>
+              <max:CREATIONDATE changed=""?"">" + dateString + @"</max:CREATIONDATE>
+              <max:CHANGEDATE changed=""?"">" + dateString + @"</max:CHANGEDATE>
+              <max:CLASSSTRUCTUREID changed=""?"">" + classid + @"</max:CLASSSTRUCTUREID>
+               <!--Optional:--> 
+            </max:SR> 
+         </max:MXSRSet> 
+      </max:CreateMXSR> 
+   </soapenv:Body> 
+</soapenv:Envelope>";
+                bytes = System.Text.Encoding.ASCII.GetBytes(requestXml);
+                wbRequest.ContentType = "text/xml; encoding='utf-8'";
+                wbRequest.ContentLength = bytes.Length;
+                wbRequest.Method = "POST";
+                ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback (   delegate { return true; });
+
+                Stream requestStream = wbRequest.GetRequestStream();
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+
+                string strResponse = "";
+                try
+                {
+                    using (WebResponse webResponse = wbRequest.GetResponse())
+                    {
+                        var streamReader = new StreamReader(webResponse.GetResponseStream());
+                        strResponse = streamReader.ReadToEnd();
+                        string TicketID = GetIDFromXml(strResponse);
+                        return TicketID;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex ;
+            }
+            finally
+            {
+
+            }
+        }
+
+
+     
+}
---
+++ TivoliDB_commands.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Dapper;
+using System.Data;
+using System.Configuration;
+using System.Data.OracleClient;
+using System.ServiceModel;
+using Shortcodes.MXSRRef;
+using System.IO;
+using System.Xml.Linq;
+
+/// <summary>
+/// Summary description for TivoliDB_commands
+/// </summary>
+public class TivoliDB_commands
+{
+	public TivoliDB_commands()
+	{
+		   private OracleConnection conn = new OracleConnection(ConfigurationManager.ConnectionStrings["Tivolidbdata"].ConnectionString);
+        private IDbConnection _db = new OracleConnection(ConfigurationManager.ConnectionStrings["Tivolidbdata"].ConnectionString);
+
+        public List<Models.Ticket> SearchTicket(int? scid)
+        {
+            try
+            {
+                //2400	Change Rate	CHANGE RATE
+                //2399	Remove Short Code	REMOVE SC
+                //2398	NewService_IVR_SMS	IVR_SMS
+                //2396	NewService_SMS	SMS
+                //2397	NewService_IVR	IVR
+                string commadn = @"SELECT TICKETID,ccvouchersn ,DESCRIPTION,STATUS,STATUSDATE,REPORTEDBY,REPORTDATE,CREATEDBY,CREATIONDATE FROM MAXIMO.TICKET where status  <> 'CLOSED'  and classstructureid in ('2396','2397','2398','2399','2400') and ccvouchersn  is not null ";
+                if (scid != null)
+                {
+                    string addition = " and ccvouchersn = '" + scid.ToString() + "'";
+                    return this._db.Query<Models.Ticket>(commadn + addition).ToList();
+                }
+                else
+                { return this._db.Query<Models.Ticket>(commadn).ToList(); }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+        public List<Models.Ticket> SearchTicket(bool OpenClose, DateTime? from, DateTime? to)
+        {
+            try
+            {
+                //2400	Change Rate	CHANGE RATE
+                //2399	Remove Short Code	REMOVE SC
+                //2398	NewService_IVR_SMS	IVR_SMS
+                //2396	NewService_SMS	SMS
+                //2397	NewService_IVR	IVR
+                string commadn = @"SELECT TICKETID,ccvouchersn ,DESCRIPTION,STATUS,STATUSDATE,REPORTEDBY,REPORTDATE,CREATEDBY,CREATIONDATE FROM MAXIMO.TICKET where status   " + (OpenClose == true ? " <> 'CLOSED' " : " = 'CLOSED'") + @"  and CREATIONDATE >= coalesce(:fromm,CREATIONDATE)   and 
+CREATIONDATE <=  coalesce(:too,CREATIONDATE ) and classstructureid in ('2396','2397','2398','2399','2400') and ccvouchersn  is not null order by STATUSDATE desc";
+                return this._db.Query<Models.Ticket>(commadn,new {fromm = from , too = to }).ToList(); 
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+        //public List<Models.Ticket> TicketHistory(int? scid)
+        //{
+        //    try
+        //    {
+        //        string commadn = @"SELECT TICKETID,AFFECTEDPHONE SC_ID,DESCRIPTION,STATUS,STATUSDATE,REPORTEDBY,REPORTDATE,CREATEDBY,CREATIONDATE FROM MAXIMO.TICKET where  siteid = 'site_it'";
+        //        if (scid != null)
+        //        {
+        //            string addition = " and ccvouchersn='" + scid.ToString() + "'";
+        //            return this._db.Query<Models.Ticket>(commadn + addition).ToList();
+        //        }
+        //        else
+        //        { return this._db.Query<Models.Ticket>(commadn).ToList(); }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+        //    }
+        //}
+        //select ccvouchersn from ticket where status='CLOSED'  and classstructureid in ('2385','2383','2381','2380','2386') and ccvouchersn  is not null
+        public List<Models.Ticket> TicketHistory(int scid)
+        {
+            try
+            {
+                string commadn = @"select jj.ticketid, jj.owner, jj.owndate, jj.ownerchangeby, tt.status from TKOWNERHISTORY jj join ticket tt on jj.ticketid= tt.ticketid   where tt.ticketid in (select ticketid from ticket where ccvouchersn=:sc_id )";
+
+                return this._db.Query<Models.Ticket>(commadn, new { sc_id = scid.ToString()}).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+        public List<Models.Ticket> TicketStatusHistory(int scid)
+        {
+            try
+            {
+                string commadn = @"select ticketid, Status, changeby, changedate,owner from TKSTATUS  where ticketid=(select max(tt.ticketid) from ticket tt where tt.ccvouchersn=:sc_id) order by changedate desc";
+                return this._db.Query<Models.Ticket>(commadn, new { sc_id = scid.ToString() }).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving ticket status data from tivoli database " + ex.Message);
+            }
+        }
+
+
+        public string FinishedTickets()
+        {
+            try
+            {
+                //2400	Change Rate	CHANGE RATE
+                //2399	Remove Short Code	REMOVE SC
+                //2398	NewService_IVR_SMS	IVR_SMS
+                //2396	NewService_SMS	SMS
+                //2397	NewService_IVR	IVR
+                string commadn = @"select count(*) from ticket  where status  <> 'CLOSED'  and classstructureid in ('2396','2397','2398','2399','2400') and ccvouchersn  is not null";
+
+                return this._db.Query<string>(commadn).SingleOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+
+        public string PendingInTechnicalCount()
+        {
+            try
+            {
+                string commadn = @"select count(*) from ticket  where status  <> 'CLOSED'  and classstructureid in ('2396','2397','2398','2399','2400') and ccvouchersn  is not null";
+                return this._db.Query<string>(commadn).SingleOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+        public List<int> PendingInTechnical()
+        {
+            try
+            {
+                string commadn = @"select ccvouchersn from ticket where status  <> 'CLOSED'  and classstructureid in ('2396','2397','2398','2399','2400') and ccvouchersn  is not null";
+                return this._db.Query<int>(commadn).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database" + ex.Message);
+            }
+        }
+        public List<int> PendingInTechnicalTicketNumbers()
+        {
+            try
+            {
+                string commadn = @"select ticketid from ticket where status  <> 'CLOSED'  and classstructureid in ('2396','2397','2398','2399','2400') and ccvouchersn  is not null";
+                return this._db.Query<int>(commadn).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+        //
+        public List<Models.Ticket> TicketHistoryByTicketID(int ticketid)
+        {
+            try
+            {
+                string commadn = @"select ticketid, ownergroup, owner, owndate, ownerchangeby from tkownerhistory where ticketid=:ticketid";
+                string addition = " and ccvouchersn = '" + ticketid.ToString() + "'";
+                return this._db.Query<Models.Ticket>(commadn + addition).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in retrieving data from tivoli database " + ex.Message);
+            }
+        }
+
+        public List<Models.Ticket> TicketsProgress (List<int> ticketNumbers)
+        {
+            List<Models.Ticket> tt = new List<Models.Ticket>();
+           // ('2396','2397','2398','2399','2400')
+            //2400	Change Rate	CHANGE RATE
+            //2399	Remove Short Code	REMOVE SC
+            //2398	NewService_IVR_SMS	IVR_SMS
+            //2396	NewService_SMS	SMS
+            //2397	NewService_IVR	IVR
+            string Command = @"select 
+round(
+(select count (*) from (select  distinct status from TKSTATUS where ticketid=:tkid and status like ('PEND%')))
+/
+(
+select 
+(Case  CLASSSTRUCTUREID 
+                                       when '2399' then 6
+                                       when '2400' then 4
+                                       when '2396' then 9
+                                       when '2397' then 9
+                                       when '2398' then 8 end)
+from ticket where ticketid=:tkid),2 )*100 progress, (select 
+case lower(status) 
+when 'closed' then 'progress-bar-success'
+when 'retmkt' then 'progress-bar-danger'
+when 'retra' then 'progress-bar-danger'
+when 'retra' then 'progress-bar-danger'
+else 'progress-bar-danger' end style  from ticket where ticketid=:tkid
+) style , (select description  from ticket where ticketid=:tkid)  description ,
+(select status  from ticket where ticketid=:tkid) status  from dual";
+            if (ticketNumbers.Count != 0 )
+            {
+                foreach (int i in ticketNumbers)
+                {
+                    Models.Ticket y = new Models.Ticket();
+                    y.DESCRIPTION = "";
+
+                        y = this._db.Query<Models.Ticket>(Command, new { tkid = i }).SingleOrDefault();
+                    
+                    if (String.IsNullOrEmpty(y.DESCRIPTION) == false) 
+                    {
+                        tt.Add(y);
+                    }
+                }
+            }
+            else
+            {
+                Exception ex = new Exception("No items requested for the progress bar");
+                throw ex;
+            }
+            if (tt.Count == 0)
+            {
+                Exception ex = new Exception("No items exist for the progress bar");
+                throw ex;
+            }
+            return tt;
+        }
+    }
+	}
+}

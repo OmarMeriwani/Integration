2412adabcce498865577c4ed58ca7a2daa4f9730
Omar Meriwani <omar.sirwan@gmail.com>
2017-07-26 14:46:22 +0300

    --

--- TivoliWO/TivoliWO/Controllers/HomeController.cs
+++ TivoliWO/TivoliWO/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.ServiceModel;
+using TivoliWO.ServiceReference1;
 
 namespace TivoliWO.Controllers
 {
@@ -11,14 +12,27 @@ namespace TivoliWO.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-            ServiceReference1.MXWO_WORKORDERType a = new ServiceReference1.MXWO_WORKORDERType();
-           
-            ServiceReference1.CreateMXWORequest m = new ServiceReference1.CreateMXWORequest();
-            m.MXWOSet[0] = a;
-            ServiceReference1.QueryMXWORequest mxq = new ServiceReference1.QueryMXWORequest();
+            MXWO_WORKORDERType a = new MXWO_WORKORDERType();
+            a.DESCRIPTION = new MXStringType { Value = "TEST" };
+            a.LOCATION = new MXStringType { Value = "BABEL" };
+            a.CLASSSTRUCTUREID = new MXStringType { Value = "06" };
+            a.ASSIGNEDOWNERGROUP = new MXStringType() { Value = "MKT" };
+            a.OWNERGROUP = new MXStringType() { Value = "MKT" };
             
+            MXWO_WORKORDERType[] m = new MXWO_WORKORDERType[1];
+            m[0] = a;
+            ServiceReference1.MXWOPortTypeClient MXWOPORTCLIENT = new MXWOPortTypeClient();
+            
+            CreateMXWORequest CMXWReq = new CreateMXWORequest();
+            CMXWReq.MXWOSet = m;
+            CMXWReq.creationDateTime = DateTime.Now ;
+            
+            MXWOPORTCLIENT.CreateMXWOAsync(CMXWReq);
+            //DateTime cd = DateTime.Today;
+            //string ss = "";
+            //MXWOPORTCLIENT.CreateMXWO(m,ref cd ,ref  ss,ref ss,ref ss,ref ss );
 
+            return View();
         }
 
         public ActionResult About()
